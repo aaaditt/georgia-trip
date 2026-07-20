e1d37a149ebe9a39ae3130adabe8abc4cf4d2d6a
aaaditt <aaditchandra2212@gmail.com>
2026-07-20 22:39:41 +0400

    fix: disable autocomplete on comment input

--- src/components/CommentBox.js
+++ src/components/CommentBox.js
@@ -77,6 +77,7 @@ export default function CommentBox({ comments, experienceId }) {
           value={text}
           onChange={(e) => setText(e.target.value)}
           maxLength={500}
+          autoComplete="off"
         />
         <button
           type="submit"
